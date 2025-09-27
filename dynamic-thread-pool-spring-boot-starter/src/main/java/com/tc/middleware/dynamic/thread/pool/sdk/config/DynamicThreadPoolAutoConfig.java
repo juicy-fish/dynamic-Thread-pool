d3c34e38991d3258abadac4061274684368a48c4
马鲛鱼 <1439063191@qq.com>
2025-09-27 17:32:02 +0800

    feat-第一节:搭建组件工程

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/tc/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,10 @@
+package com.tc.middleware.dynamic.thread.pool.sdk.config;
+
+/**
+ * @ClassName DynamicThreadPoolAutoConfig
+ * @Author 大马鲛鱼丸Gaga
+ * @Description 动态线程池配置的入口
+ * @Date 2025/9/27 17:27
+ */
+public class DynamicThreadPoolAutoConfig {
+}
